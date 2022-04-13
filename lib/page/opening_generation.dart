cbd81b6c3984b48799171d553d8306ba7b9c419b
DocentSzachista <idontknow5325@gmail.com>
2022-04-13 15:36:15 +0200

    Add display of last opening draw date

--- lib/page/opening_generation.dart
+++ lib/page/opening_generation.dart
@@ -1,7 +1,11 @@
+
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:chess/widgets/navigation.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../database/db_provider.dart';
 import '../models/chess_game.dart';
@@ -43,26 +47,43 @@ class _HomePageState extends State<HomePage>{
 
   final List<List< List<String>>> _move = []  ;
   final Random random = Random();
+  DateTime now = DateTime.now();
+  String _lastDate = "";
+  int _indeks = 0;
+
+
   // simple aquiring of data from database that is used in FutureBuilder class
   Future<List<Game>> _fetchFromDb() async{
     var db = ChessDatabase.instance;
     var returnedGame = await  db.readAll();
     return returnedGame;
   }
-
-  var indeks = 0;
+  void loadCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _lastDate = (prefs.getString('lastDate') ?? "");
+    });
+  }
+  @override
+  void initState(){
+    super.initState();
+    loadCounter();
+  }
 
   // TODO: make some sort of generation engine
-  void onClick(){
+  void  onClick() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      indeks = random.nextInt(_move.length);
+      _indeks = random.nextInt(_move.length);
+      _lastDate = DateFormat("yyyy-MM-dd").format(now);
+      prefs.setString("lastDate", _lastDate);
+      print(_lastDate);
     });
   }
   @override
   Widget build(BuildContext context) {
 
     return FutureBuilder(
-
         future: _fetchFromDb(),
         builder: (context, snapshot) {
           Widget child;
@@ -71,13 +92,15 @@ class _HomePageState extends State<HomePage>{
             for(var game in gamesList){
               _move.add(game.convertForAppTable());
             }
-            child = MoveTable( moves:  _move[indeks]);
+            child = MoveTable( moves:  _move[_indeks]);
           }
           else
           {
             child = MyProgressIndicator();
           }
           return Column( children: <Widget>[
+            Text("${AppLocalizations.of(context)!.lastDraw}: ${_lastDate}",
+            style: TextStyle(fontSize: 20)),
             child,
             TextButton(
               onPressed: onClick,
@@ -88,7 +111,7 @@ class _HomePageState extends State<HomePage>{
                 backgroundColor: Colors.grey,
                 textStyle: const TextStyle(fontSize: 20),
               ),
-            )
+            ),
           ])
 
           ;
